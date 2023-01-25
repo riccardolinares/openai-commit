c9f3ef8c9e97047b4ab2d9679dce951fd9c744c1
riccardo.linares <linares.riccardo@gmail.com>
2023-01-25 16:50:44 +0100

    testing special char

--- src/main.ts
+++ src/main.ts
@@ -12,25 +12,27 @@ export async function run(): Promise<void> {
 
     core.debug(`openai-prompt: ${core.getInput("openai-prompt")}`);
 
-    const response = await openai.createCompletion({
-      model: core.getInput("model"),
-      prompt: parseCommit(core.getInput("openai-prompt")),
-      temperature: 0.7,
-      max_tokens: 256,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-    const text = response.data.choices[0].text ?? "";
+    // const response = await openai.createCompletion({
+    //   model: core.getInput("model"),
+    //   prompt: parseCommit(core.getInput("openai-prompt")),
+    //   temperature: 0.7,
+    //   max_tokens: 256,
+    //   top_p: 1,
+    //   frequency_penalty: 0,
+    //   presence_penalty: 0,
+    // });
+    // const text = response.data.choices[0].text ?? "";
 
 	// Escape the text to avoid breaking the YAML
-	core.debug(`text: ${text.trim().replace(/(\r\n|\n|\r)/gm,"").replace(/'/g, "\\'").replace(/"/g, '\\"').replace(/`/g, "\\`")}`);
+	// core.debug(`text: ${text.trim().replace(/(\r\n|\n|\r)/gm,"").replace(/'/g, "\\'").replace(/"/g, '\\"').replace(/`/g, "\\`")}`);
 
 
     // core.debug(`openai-response: ${text}`);
 
+	const text = "This is a test \n\r in case of special char ' \" qualcosa ' succederà ééé % \\ ";
+
     // The output of this action is the body of the tweet
-    // core.setOutput('text', text);
+    core.setOutput('text', text);
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message);
